0e5a9d1b01e56a79be32d81ef18d6f65ccf65657
Zachary Kramer <zckramer83@gmail.com>
2020-04-30 17:53:36 -0400

    can now add american cheese to the recipe

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import Nutrient from './Nutrient'
 
 
 export default function Ingredient (props) {
 
     return (
         <div className="Ingredient">
-            <div className="Ingredient__Name">{props.label}</div>
-    <div className="Ingredient__Serving">{props.measurement} {props.unit}</div>
-            <hr />
-            <Nutrient label="Fat" value="2" measurement="g" index="1"></Nutrient>
-            <hr />
-            <Nutrient label="Calories" value="150" measurement="g" index="2"></Nutrient>
-            <hr />
-            <Nutrient label="Carbs" value="15" measurement="g" index="3"></Nutrient>
-            <hr />
+            <div className="Ingredient__Name">{props.label}
+                <div className="Ingredient__Serving">{props.measurement} {props.unit}
+                    <hr />
+                    <span className="Nutrient-Label__Fat">{props.fat} fat</span>
+                    <hr />
+                    <span className="Nutrient-Label__Carbs">{props.carbs} carbs</span>
+                    <hr />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Recipe-Card.js
+++ src/components/Recipe-Card.js
@@ -6,39 +6,63 @@ export default function RecipeCard (props) {
     
     const banana = {label: "Banana", measurement: "1", unit: "whole", fat: "1g", carbs: "25g" }
     const almondMilk = {label: "Almond Milk", measurement: "1", unit: "cup", fat: "0g", carbs: "2g" }
-    const yoghurt = {label: "Yoghurt", measurement: "1", unit: "1/4 cup", fat: "6g", carbs: "15g" }
+    const yoghurt = {label: "Yoghurt", measurement: "1", unit: "quarter cup", fat: "6g", carbs: "15g" }
+    
+    // JSON data to fetch
+    
+    const americanCheese = {label: "American Cheese", measurement: "1", unit: "slice", fat: "8g", carbs: "2g" }
     
     // States
-    const [recipe, setRecipe] = React.useState()
-    const [nutArray, setNutArray] = React.useState([banana, almondMilk, yoghurt])
+
+    const [ingredientsArr, setIngredientsArray] = React.useState([banana, almondMilk, yoghurt])
     
-    function sumNutrient (nutArr, prop) {
+    // Functions
+
+    function sumNutrient (array, prop) {
         let total = 0
         
-        nutArr.forEach(nutrient=>{
-            total = total + parseInt(nutrient[prop])
-        })
+        if(Array.isArray(array)){
+            array.forEach(nutrient=>{
+                total = total + parseInt(nutrient[prop])
+            })
+        }
         
         return total
     }
     
-    
+    function handleAddIngredient (newIngredient) {
+        console.log(ingredientsArr)
+        setIngredientsArray(ingredientsArr.concat(americanCheese))
+    }
 
 
     
     return (
         <div className="Recipe-Card">
-            <h2 className="Recipe-Card__Title" >Banana Smoothie!</h2>
             <div className="Recipe-Card__Header">
-                <span className="Recipe-Card__Label"></span>        
-                <span className="Recipe-Card__Data"></span>
+                <h2 className="Recipe-Card__Label">Banana Smoothie!</h2>
             </div>
             <div className="Recipe-Card__Totals">
-                <div>{sumNutrient(nutArray, "fat")} grams of fat</div>
-                <div>{sumNutrient(nutArray, "carbs")} grams of carbs</div>
+                <div>{sumNutrient(ingredientsArr, "fat")} grams of fat</div>
+                <div>{sumNutrient(ingredientsArr, "carbs")} grams of carbs</div>
             </div>
             <div className="RecipeCard__Ingredients-Container">
-                <Ingredient 
+
+                {ingredientsArr.map((ingredient, index)=>{
+                        return( 
+                            <Ingredient 
+                                key={index}
+                                label={ingredient.label}
+                                measurement={ingredient.measurement} 
+                                unit={ingredient.unit}
+                                fat={ingredient.fat}
+                                carbs={ingredient.carbs}
+                            />
+                        ) 
+                    })
+                }
+                
+                {/* <Ingredient 
                     label={banana.label}
                     measurement={banana.measurement} 
                     unit={banana.unit}
@@ -61,7 +85,14 @@ export default function RecipeCard (props) {
                     fat={yoghurt.fat}
                     carbs={yoghurt.carbs}
 
-                />
+                /> */}
+                <div
+                onClick={()=>{
+                    handleAddIngredient(americanCheese)}}>
+                    Add an Ingredient
+
+                </div>
+
             </div>
         </div>
     )
